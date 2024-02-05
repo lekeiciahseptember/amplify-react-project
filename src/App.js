47cde3e6ab43dedcf7024f0655a4aa625063cedd
lekeiciahseptember <lesepte022@student.wethinkcode.co.za>
2024-02-05 16:34:31 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lekeiciah Testing Amplify</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App;
+  export default App;
